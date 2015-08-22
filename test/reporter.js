5a2ac2aa5a7905038cd0808b1ee5c55e9deb80a9
Markus Felten <Markus.Felten@gmx.de>
2015-08-22 16:33:35 +0200

    test(scopeReporter): enter/leave scope

--- test/reporter.js
+++ test/reporter.js
@@ -13,14 +13,31 @@ const sc = require('../lib/scopeReporter.js');
 
 const scopes = {
   'file' : { properties: { name : {} }, format: "file: ${name}" },
-  'line' : { properties: { number : {} }, format: "line: ${number}" }
+  'line' : { parent: 'file', properties: { number : {} }, format: "${file.name}/${number}" },
+  'function' : { properties: { name: {} }, format: "function: ${name}"}
 };
 
 describe('reporter', function () {
   describe('create', function () {
+    const reporter = sc.createReporter(scopes);
+
     it('scope stack empty', function () {
-      const reporter = sc.createReporter(scopes);
       assert.lengthOf(reporter.scopeStack, 0);
     });
   });
+
+  describe('enter leave scopes', function () {
+    const reporter = sc.createReporter(scopes);
+
+    it('can enter scope', function () {
+      reporter.enterScope('file', { name: 'aFile'});
+      assert.lengthOf(reporter.scopeStack, 1);
+    });
+
+    it('can leave scope', function () {
+      reporter.leaveScope();
+      assert.lengthOf(reporter.scopeStack, 0);
+    });
+  });
+
 });
